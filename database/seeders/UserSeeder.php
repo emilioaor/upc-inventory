f4bcc912d7bcdf1d7bc7840d9936fd6e9ccebcf0
Emilio Ochoa <app@sayyezz.com>
2021-02-10 10:18:50 -0400

    Initial commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        $user->email = 'emilioaor@gmail.com';
+        $user->name = 'Emilio Ochoa';
+        $user->password = bcrypt('123456');
+        $user->role = User::ROLE_ADMIN;
+        $user->save();
+
+        $user = new User();
+        $user->email = 'jlzreik@yezzcorp.com';
+        $user->name = 'Jose Luis Zreik';
+        $user->password = bcrypt('123456');
+        $user->role = User::ROLE_ADMIN;
+        $user->save();
+
+        if (config('app.env') !== 'production') {
+            $user = new User();
+            $user->email = 'inventory@mail.com';
+            $user->name = 'User Inventory';
+            $user->password = bcrypt('123456');
+            $user->role = User::ROLE_INVENTORY;
+            $user->save();
+
+            $user = new User();
+            $user->email = 'warehouse@mail.com';
+            $user->name = 'User Warehouse';
+            $user->password = bcrypt('123456');
+            $user->role = User::ROLE_WAREHOUSE;
+            $user->save();
+        }
+    }
+}
